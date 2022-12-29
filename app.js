3ae84cce748e62aab03ac5f1b76ef99a24c10812
pAsta-kun <112457969+pAsta-kun@users.noreply.github.com>
2022-12-29 11:29:46 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,27 +2,48 @@ let counter = 0;
 function changeColor()
 {
     counter++;
+    window.localStorage.setItem('newCounter', counter);
     
     if(counter % 2 != 0)
     {
-        document.body.style.color = "#3f3f3f";
-        document.body.style.background = "#f7f7f7";
-        document.body.style.transition = "500ms ease-in";
-        document.getElementById('modeBtn').style.background = "#f7f7f7";
+
+        document.getElementById('colorMode').style.color = "#3f3f3f";
         document.getElementById('modeBtn').style.color = "#3f3f3f";
-        document.getElementById('footer').style.color = "#f7f7f7";
+        document.getElementById('colorMode').style.background = "#f7f7f7";
+        document.getElementById('modeBtn').style.background = "#f7f7f7";
+        document.getElementById('colorMode').style.transition = "500ms ease-in";
         document.getElementById('modeBtn').style.transition = "500ms ease-in";
+
     }
     else
     {
-        document.body.style.color = "#f7f7f7";
-        document.body.style.background = "#3f3f3f";
+        document.getElementById('colorMode').style.color = "#f7f7f7";
+        document.getElementById('colorMode').style.background = "#3f3f3f";
         document.getElementById('modeBtn').style.background = "#3f3f3f";
         document.getElementById('modeBtn').style.color = "#f7f7f7";
-        document.getElementById('footer').style.color = "#3f3f3f";
-        document.getElementById('modeBtn').style.transition = "500ms ease-in";
+        document.getElementById('colorMode').style.transition = "500ms ease-in";
     }
         
+}
+function courseColorChange()
+{
+    let counter = localStorage.getItem('newCounter');
+    if(counter % 2 != 0)
+    {
+
+        document.getElementById('colorMode').style.color = "#f7f7f7";
+        document.getElementById('colorMode').style.background = "#546fdb";
+        document.getElementById('colorMode').style.transition = "500ms ease-in";
+        document.body.style.background = "#f7f7f7";
+        document.body.style.transition = "500ms ease-in";
+
+    }
+    else
+    {
+        document.getElementById('colorMode').style.color = "#f7f7f7";
+        document.getElementById('colorMode').style.background = "#3f3f3f";
+        document.getElementById('colorMode').style.transition = "500ms ease-in";
+    }
 }
 
 const observer = new IntersectionObserver((entries) => {
