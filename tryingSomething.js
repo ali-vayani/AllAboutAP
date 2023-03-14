0e2c8714071a2a2e155dd14e9b1c9b605b9cb8d5
pAsta-kun <112457969+pAsta-kun@users.noreply.github.com>
2023-03-14 14:34:18 -0500

    i did something

--- tryingSomething.js
+++ tryingSomething.js
@@ -1,2 +1,14 @@
-let myData = JSON.parse(tryingSomething);
-console.log(myData);
+let apLangComp = 
+{
+    "name": "AP English Language and Composition",
+    "courseInfo": "idk",
+    "examDate": "Tuesday, May 9th 8:00 AM",
+    "mcqInfo": "45 Questions | 1 hour | 45% of the total score"
+
+};
+
+let title = document.getElementsByClassName('articleHeader');
+function settingText()
+{
+    title[0].innerHTML = apLangComp["name"];
+}
